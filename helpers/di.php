f95df82d4cbdc7119e3d1b3da3798b4283f1bb87
Daniel Doyle <tr33m4n@googlemail.com>
2021-08-06 21:54:19 +0100

    Use static variable to initialise container

--- helpers/di.php
+++ helpers/di.php
@@ -1,29 +1,24 @@
 <?php
+
 declare(strict_types=1);
 
 use tr33m4n\Di\Container;
-use tr33m4n\Utilities\Registry;
 
 /**
  * Helper function for easily accessing the DI container
  *
- * @throws \tr33m4n\Utilities\Exception\RegistryException
  * @return \tr33m4n\Di\Container
  */
 function di() : Container
 {
-    if (Registry::has(Container::REGISTRY_NAMESPACE)) {
-        return Registry::get(Container::REGISTRY_NAMESPACE);
+    static $container = null;
+    if ($container instanceof Container) {
+        return $container;
     }
 
-    Registry::set(
-        Container::REGISTRY_NAMESPACE,
-        new Container(
-            new Container\ClassParameterResolver(),
-            new Container\PreferenceResolver(),
-            new Container\SharedResolver()
-        )
+    return $container = new Container(
+        new Container\ClassParameterResolver(),
+        new Container\PreferenceResolver(),
+        new Container\SharedResolver()
     );
-
-    return Registry::get(Container::REGISTRY_NAMESPACE);
 }
